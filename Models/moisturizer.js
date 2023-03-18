ee1b247c3ed80888ebc102510c49237a310bb089
flo777 <fkaminskaya21@gmail.com>
2023-03-17 21:58:15 -0600

    First Model made and seeded to database sucess

--- Models/moisturizer.js
+++ Models/moisturizer.js
@@ -1,3 +1,13 @@
 import mongoose from '../db/connection.js'
 
 
+const moisturizerSchema = new mongoose.Schema({
+    
+    brand_name: String,
+    item_name: String,
+    base_price: String,
+    high_price: String,
+
+})
+
+export default mongoose.model("moisturizer", moisturizerSchema)
--- db/seed.js
+++ db/seed.js
@@ -1,3 +1,14 @@
 //import models
+import moisturizer from '../Models/moisturizer.js';
 //import data from json 
-//insert data into the database with model
+import moisturizerData from "../sephora-moisturizer-2023.json" assert {type: "json" };
+//insert data into the database with models
+
+async function seed() {
+    await moisturizer.deleteMany({})
+
+    await moisturizer.create(moisturizerData)
+    
+    process.exit()
+}
+seed()
